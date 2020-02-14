c9dd2b302d2b44926b57c2a98bbeda943de6a425
Terens Tare <terens.t17@gmail.com>
2020-02-14 19:03:20 +0100

    Fixed project infrastructure

---
+++ include/exp/Easingsv2.hpp
@@ -0,0 +1,15 @@
+#ifndef EASINGS_V2_HPP
+#define EASINGS_V2_HPP
+
+namespace tnt
+{
+class Easing
+{
+public:
+    virtual ~Easing() noexcept {}
+
+    virtual void Run(float time, float begin, float change, float duration) = 0;
+};
+} // namespace tnt
+
+#endif //!EASINGS_V2_HPP
